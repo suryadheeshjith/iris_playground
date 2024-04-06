0accb5692be35c6e59f509125c14029fe50de8fd
Surya <surya.dheeshjith@gmail.com>
2024-04-06 18:54:39 -0400

    Added state space yamls

--- scripts/local.sh
+++ scripts/local.sh
@@ -7,7 +7,7 @@ comp="compute=local"
 # GO BOTTOM TO TOP
 
 # local train
-# ./.python-greene submitit_hydra.py $comp exp=trainer name="$(date +%F)-trainer_test"
+./.python-greene submitit_hydra.py $comp exp=trainer name="$(date +%F)-trainer_test_assault"
 
 # local eval
-./.python-greene submitit_hydra.py $comp exp=eval name="$(date +%F)-eval_test"
+# ./.python-greene submitit_hydra.py $comp exp=eval name="$(date +%F)-eval_test"
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-./.python-greene submitit_hydra.py compute/greene=1x1 compute/greene/node=rtx8000 exp=trainer name="$(date +%F)-1GPU_train_img_breakout"
+./.python-greene submitit_hydra.py compute/greene=1x1 compute/greene/node=rtx8000_6hrs exp=trainer name="$(date +%F)-1GPU_train_state_assault"
